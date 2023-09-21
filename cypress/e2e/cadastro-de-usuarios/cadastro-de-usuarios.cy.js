8254e7e191f8a5cc70c4c2152ad6ae5f8e7d77e3
Nataly Lucena <natalylucena.pb@gmail.com>
2023-09-21 16:25:08 -0300

    Projeto criado

---
+++ cypress/e2e/cadastro-de-usuarios/cadastro-de-usuarios.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Cenário Cadastro de Usuários', () => {
+    beforeEach(() => {
+      
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    })
+  
+    it('Validar Criação da Senha do Usuário', () => {
+      
+    })
+
+    it('Validar Registro de Usuário com username que já existe', () => {
+      
+    })
+})
---
+++ cypress/e2e/login/login.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Cenário Login de Usuários', () => {
+  
+  beforeEach(() => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') 
+  });
+
+  
+  it('Realizar o login com sucesso', () => {
+    
+  });
+
+  // Teste para realizar o login com usuário e senha incorretos
+  it('Exibir uma mensagem de erro ao inserir credenciais incorretas', () => {
+    
+  });
+});
