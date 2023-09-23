e98670101f3e98d0749cb78b7189d2c7d951da1d
Liedson <liedsonkauam01@gmail.com>
2023-09-23 09:12:11 -0300

    última modificação

---
+++ cypress/e2e/busca-de-usario/busca-de-usuarios.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+
+
+
+describe ('Busca de usuários', () => {
+
+    beforeEach(() => {  
+        cy.username('Admin');
+        cy.password('admin123')
+    })
+
+     it('Busca de usuário com dados válidos', () => {
+        cy.searchUsername().type('Anthony.Nolan');
+        cy.selectUserType()
+        cy.name()
+        cy.Status()
+        cy.get('.oxd-form-actions > .oxd-button--secondary').click({force:true})
+        cy.wait(3000)
+
+    })
+
+it('Busca de usuário com dados inválidos', () => {
+    cy.searchUsername().type('Zé.da.Manga')
+    cy.selectUserType()
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(3) > div > div:nth-child(2) > div > div > input').type('Zé da Manga')
+    cy.wait(1000)
+    cy.get('.oxd-autocomplete-option').click();
+    cy.Status()
+    cy.get('.oxd-form-actions > .oxd-button--secondary').click({force:true})
+    cy.wait(1000)
+
+
+
+
+
+
+
+    })
+})
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,51 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+require('cypress-xpath');
+
+Cypress.Commands.add('forceClick', {prevSubject: 'element'}, (subject, options) => {
+    cy.wrap(subject).click({force: true});
+  });
+
+Cypress.Commands.add('username', (usuario) => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers');
+    cy.get('input[name="username"]').should('be.enabled').click().type(usuario);
+    
+});
+
+Cypress.Commands.add('password', (senha) => {
+    cy.get('input[name="password"]').should('be.enabled').click().type(senha);
+    cy.get('button[type="submit"]').should('have.class','oxd-button oxd-button--medium oxd-button--main orangehrm-login-button').click();
+});
+
+Cypress.Commands.add('abaAdmin', () => {
+    cy.get('button[class="oxd-main-menu-item"]').click();
+});
+
+Cypress.Commands.add('searchUsername', () => {
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(1) > div > div:nth-child(2) > input').click();
+});
+
+Cypress.Commands.add('selectUserType', () => {
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(2) > div > div:nth-child(2) > div > div').click();
+    cy.get('.oxd-select-dropdown > :nth-child(3)').click();
+
+});
+        
+Cypress.Commands.add('name', () => {
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(3) > div > div:nth-child(2) > div > div > input').type('Anthony Nolan')
+    cy.wait(2000);
+    cy.get('.oxd-autocomplete-option').click();
+});
+
+Cypress.Commands.add('nameSelect', () => {
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(3) > div > div:nth-child(2) > div > div');
+});
+
+Cypress.Commands.add('Status', () => {
+    cy.get('#app > div.oxd-layout > div.oxd-layout-container > div.oxd-layout-context > div > div.oxd-table-filter > div.oxd-table-filter-area > form > div.oxd-form-row > div > div:nth-child(4) > div > div:nth-child(2) > div > div').click();
+    cy.get('.oxd-select-dropdown > :nth-child(2)').click();
+    cy.wait(1000);
+});
+
